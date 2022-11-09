8e8bfe07a287f7ed135ffd618dd0b2497cd592e8
ersankarimi <ersankarimii311@gmail.com>
2022-11-09 21:56:29 +0800

    feat: setup the Routes
    Setup the routes will be render in the web using createBrowserRouter.
    
    Wrapped the whole web with RouterProvider from react-router-dom and set router value according to router variable.
    
    The root path will be render the App component and the index element is Homepage route

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Homepage from "./routes/homepage";
 import App from "./App";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

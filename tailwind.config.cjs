f9ae312e6fe67fa1c01e217c03055c6885e6f833
ersankarimi <ersankarimii311@gmail.com>
2022-11-11 00:05:00 +0800

    feat: extend the default colors and background image property Tailwind CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,15 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
+      colors: {
+        "neutral-custom": {
+          100: "#effaff",
+        },
+      },
+      backgroundImage: {
+        "main-color":
+          "linear-gradient(180deg, #1F2937 0%, #27303F 29.17%, #2E3846 55.21%, #363F4E 77.6%, #3E4756 97.92%)",
+      },
       fontFamily: {
         roboto: "'Inter', sans-serif",
         kanit: "'Kanit', sans-serif",

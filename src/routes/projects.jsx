d5c9aa7d4e5b7ffe9c6e84c1426d2e2bad2adc4f
ersankarimi <ersankarimii311@gmail.com>
2022-11-11 00:04:21 +0800

    feat: created Projects routes components

---
+++ src/routes/projects.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async";
+
+export default function Projects() {
+  return (
+    <>
+      <Helmet>
+        <title>Projects | Ersan Karimi</title>
+      </Helmet>
+      <h1>Projects</h1>
+    </>
+  );
+}

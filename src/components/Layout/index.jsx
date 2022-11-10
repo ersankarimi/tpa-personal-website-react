ceb6478c4e9c9c8fa1cc32228361e53ad60efc08
ersankarimi <ersankarimii311@gmail.com>
2022-11-10 23:54:04 +0800

    refactor: add some classes style Tailwind CSS
    Add the padding classes style for the main element

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,7 +5,9 @@ export default function RootLayout({ children }) {
   return (
     <>
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 px-4 py-3 md:px-12 lg:py-4 xl:px-24 2xl:px-28">
+        {children}
+      </main>
       <Footer />
     </>
   );

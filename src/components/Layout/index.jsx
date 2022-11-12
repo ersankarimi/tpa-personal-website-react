d911a05e66536f0415b8c0665697b33ef2868e77
ersankarimi <ersankarimii311@gmail.com>
2022-11-12 14:38:20 +0800

    refactor: add some contents

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,3 +1,5 @@
+import { avatarHero, ellipseHero } from "../../assets";
+
 import Footer from "./footer";
 import Header from "./header";
 
@@ -5,7 +7,21 @@ export default function RootLayout({ children }) {
   return (
     <>
       <Header />
-      <main className="flex-1 px-4 py-3 md:px-12 lg:py-4 xl:px-24 2xl:px-28">
+      <main className="mt-12 flex-1 px-6 py-3 md:px-12 lg:py-4 xl:px-24 2xl:px-28">
+        <section className="my-8">
+          <div className="group relative flex  items-center justify-center p-8">
+            <img
+              src={ellipseHero}
+              alt="Ellipse Illustration"
+              className="absolute -top-8 bottom-0 -z-[1] bg-cover bg-no-repeat object-cover duration-200 ease-in group-hover:rotate-45 group-hover:scale-110"
+            />
+
+            <picture className="flex justify-center duration-300 group-hover:scale-105">
+              <source media="(min-width:360px)" srcSet={avatarHero} />
+              <img src={avatarHero} alt="Ersan Profile" />
+            </picture>
+          </div>
+        </section>
         {children}
       </main>
       <Footer />

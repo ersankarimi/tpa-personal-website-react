fc12fae9b76bb8de8eb6d74fe6dcd102e30290f7
ersankarimi <ersankarimii311@gmail.com>
2022-11-12 20:03:25 +0800

    feat: import and re-export `Section` component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Buttons/button";
 import ButtonSocialMedia from "./Buttons/button-social-media";
 import BlogCard from "./blog-card";
 import RootLayout from "./Layout";
+import Section from "./section";
 
-export { BlogCard, Button, ButtonSocialMedia, RootLayout };
+export { BlogCard, Button, ButtonSocialMedia, RootLayout, Section };

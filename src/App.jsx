27c823bbd7c9b2a27e1fafd60e4b84497fbcd34d
ersankarimi <ersankarimii311@gmail.com>
2022-11-12 22:31:54 +0800

    feat: add `ScrollRestoration`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 import { Root } from "./routes";
 
 export default function App() {
   return (
     <Root>
+      <ScrollRestoration />
       <Outlet />
     </Root>
   );
